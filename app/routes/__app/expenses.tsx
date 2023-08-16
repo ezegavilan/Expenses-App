2a9b33e67a1508aa39fc5e62fd8acdec144a0850
Ezequiel Gerardo Gavilan <eggavilan@teco.com.ar>
2023-08-16 17:23:21 -0300

    Adding button to add new expenses & to download raw data

--- app/routes/__app/expenses.tsx
+++ app/routes/__app/expenses.tsx
@@ -1,7 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import {Link, Outlet} from "@remix-run/react";
 import ExpensesList from "~/components/expenses/ExpensesList";
 import {EXPENSES} from "~/routes/__app/expenses.analysis";
+import {FaDownload, FaPlus} from "react-icons/fa";
 
 export const meta: MetaFunction = () => {
     return { title: 'Expenses | Your expenses' }
@@ -12,6 +13,16 @@ export default function ExpensesLayout() {
         <>
             <Outlet />
             <main>
+                <section id="expenses-actions">
+                    <Link to='add'>
+                        <FaPlus />
+                        <span>Add expense</span>
+                    </Link>
+                    <a href="/expenses/raw">
+                        <FaDownload />
+                        <span>Load Raw Data</span>
+                    </a>
+                </section>
                 <ExpensesList expenses={ EXPENSES } />
             </main>
         </>
