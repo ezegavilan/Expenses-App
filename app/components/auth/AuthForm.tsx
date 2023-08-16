6ed989ec02487597c961093464c26eb2c99d8eb7
Ezequiel Gerardo Gavilan <eggavilan@teco.com.ar>
2023-08-16 17:18:29 -0300

    signup/login page

--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,10 +1,22 @@
-import { FaLock } from 'react-icons/fa';
+import { Link, useSearchParams } from "@remix-run/react";
+import {FaLock, FaUserPlus} from 'react-icons/fa';
 
 function AuthForm() {
+  const [searchParams] = useSearchParams();
+  const authMode = searchParams.get('mode') || 'login';
+
+  const submitBtnCaption: string = (authMode === 'login') ? 'Login' : 'Sign Up';
+  const toggleBtnCaption: string = (authMode === 'login') ? 'Create a new user' : 'Log in with existing user';
+
   return (
     <form method="post" className="form" id="auth-form">
       <div className="icon-img">
         <FaLock />
+          {
+              (authMode === 'login')
+                  ? <FaLock />
+                  : <FaUserPlus />
+          }
       </div>
       <p>
         <label htmlFor="email">Email Address</label>
@@ -15,8 +27,8 @@ function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button>Login</button>
-        <a href="/__marketing/auth">Log in with existing user</a>
+        <button>{ submitBtnCaption }</button>
+        <Link to={ (authMode === 'login') ? '?mode=signup' : '?mode=login' }>{ toggleBtnCaption }</Link>
       </div>
     </form>
   );
